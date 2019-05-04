87221e1ec40bb4bd6bfd34fea6478d35ecee51ff
Diego Teixeira <diegoteixeir4@gmail.com>
2019-05-04 01:40:08 -0300

    notify-on-new-file

---
+++ notify-on-new-file.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# notify-on-new-file.sh
+#
+# This script watches a directory passed as argument and shows a
+# desktop notification when a file is created or moved to it.
+# Notification title is the first line of the new file and
+# the body is the other lines. After it is displayed the file is
+# removed.
+#
+# Usage:
+#   ./notify-on-new-file.sh [directory]
+#
+# Example:
+#   ./notify-on-new-file.sh ./tmp
+#   echo -e "Hello, world\nMy first notification." > ./tmp/1.txt
+
+watched_dir=$1
+
+# inotifywait is part of inotify-tools package on Debian,
+# to install it use:
+#   sudo apt install inotify-tools
+
+inotifywait -m -q -e create -e moved_to $watched_dir |
+  while read path action file; do
+    file_path="$watched_dir/$file"
+    title=$(head -n 1 $file_path)
+    body=$(tail -n +2 $file_path)
+    notify-send -t 5000 "$title" "$body"
+    rm $file_path
+  done
+
